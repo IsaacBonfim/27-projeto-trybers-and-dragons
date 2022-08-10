229472d016505c14c6368d12834dcd759fe959a8
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-10 02:00:18 -0300

    feat: Criado o arquivo Necromancer.ts contendo uma sub classe de Archetype

--- src/Archetypes/Necromancer.ts
+++ src/Archetypes/Necromancer.ts
@@ -1,7 +1,22 @@
 import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
 class Necromancer extends Archetype {
+  private _energy: EnergyType = 'mana';
+  private static instances = 0;
 
+  constructor(name: string) {
+    super(name);
+    Necromancer.instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
 }
 
 export default Necromancer;
