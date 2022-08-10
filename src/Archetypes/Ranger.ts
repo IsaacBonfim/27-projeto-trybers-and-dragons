e58de59bd92ac197ee6f9ab085e13b994aca9197
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-10 02:02:26 -0300

    feat: Criado os arquivos Warrior.ts e Ranger.ts contendo uma subclasse de Archetype

--- src/Archetypes/Ranger.ts
+++ src/Archetypes/Ranger.ts
@@ -1,7 +1,22 @@
 import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
 class Ranger extends Archetype {
+  private _energy: EnergyType = 'stamina';
+  private static instances = 0;
 
+  constructor(name: string) {
+    super(name);
+    Ranger.instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
 }
 
 export default Ranger;
--- src/Archetypes/Warrior.ts
+++ src/Archetypes/Warrior.ts
@@ -1,7 +1,22 @@
 import Archetype from './Archetype';
+import { EnergyType } from '../Energy';
 
 class Warrior extends Archetype {
+  private _energy: EnergyType = 'stamina';
+  private static instances = 0;
 
+  constructor(name: string) {
+    super(name);
+    Warrior.instances += 1;
+  }
+
+  static createdArchetypeInstances(): number {
+    return this.instances;
+  }
+
+  get energyType(): EnergyType {
+    return this._energy;
+  }
 }
 
 export default Warrior;
