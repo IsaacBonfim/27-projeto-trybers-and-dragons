0fb95fe4eaa13cb26f7d96098577f65a643fb25a
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-08-10 19:59:57 -0300

    feat: Criado o arquivo index.ts na pasta raiz do projeto. Enviando o requisito 13

---
+++ src/index.ts
@@ -0,0 +1,38 @@
+import Character from './Character';
+import Monster from './Monster';
+import Dragon from './Dragon';
+import Battle, { PVP, PVE } from './Battle';
+import getRandomInt from './utils';
+
+const player1 = new Character('player1');
+const player2 = new Character('player2');
+const player3 = new Character('player3');
+
+const aux = getRandomInt(1, 5);
+
+for (let index = 0; index < aux; index += 1) {
+  player1.levelUp();
+}
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+const pve = new PVE(player1, [monster1, monster2]);
+
+function runBattles(battles: Battle[]): void {
+  battles.forEach((battle) => {
+    battle.fight();
+  });
+}
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
